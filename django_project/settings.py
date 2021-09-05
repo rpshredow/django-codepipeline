88bc8bb449018b3b38e96c4d6fd90ef0f8575492
Robert <rshredow@gmail.com>
2021-09-05 09:41:28 -0700

    media

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
