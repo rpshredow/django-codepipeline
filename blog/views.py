0e176e01d960bf44bdff64c848e904dbe06facbf
Robert <rshredow@gmail.com>
2021-09-05 16:08:14 -0700

    html layout and some css

--- blog/views.py
+++ blog/views.py
@@ -12,15 +12,12 @@ from .models import Post
 
 
 def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
-
+    return render(request, 'blog/home.html', {'title': 'Home'})
+    
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    template_name = 'blog/blog.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 5
