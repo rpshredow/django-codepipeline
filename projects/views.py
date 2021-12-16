d204f69911356e322694518d5e241de120eb0d76
Robert <rshredow@gmail.com>
2021-12-15 23:19:53 -0800

    dec 15

--- projects/views.py
+++ projects/views.py
@@ -1,26 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
-from django.http import JsonResponse
-#from .models import Post
+from django.http import JsonResponse, HttpResponse
 
-import requests
+#from .models import Post
 import json
-    
+
+from PIL import Image
+import requests, math
+from io import BytesIO
+
+
 def terrain(request):
     return render(request, 'projects/terrain.html', {'title': '3D Terrain App'})
+
 
 def cluster(request):
     return render(request, 'projects/cluster.html', {'title': 'Raspberry Pi Cluster'})
 
+
 def thesis(request):
     return render(request, 'projects/thesis.html', {'title': 'Masters Thesis'})
+
 
 def fpgann(request):
     return render(request, 'projects/fpgann.html', {'title': 'FPGA Neural Network'})
 
+
 def stock(request):
     return render(request, 'projects/stock.html', {'title': 'Show stock price'})
+
 
 def price(request):
     stock = request.GET['stock']
@@ -33,7 +42,7 @@ def price(request):
 
     full_url = endpoint.format(stock_ticker=stock)
 
-    page = requests.get(url=full_url, params={'apikey' : td_consumer_key})
+    page = requests.get(url=full_url, params={'apikey': td_consumer_key})
 
     content = json.loads(page.content)
 
@@ -56,8 +65,7 @@ def price(request):
     divYield = data['divYield']
     divDate = data['divDate']
 
-
-    return render(request, 'projects/result.html', {
+    return render(request, 'projects/price.html', {
         'symbol': symbol,
         'description': description,
         'bidPrice': bidPrice,
@@ -74,4 +82,38 @@ def price(request):
         'divAmount': divAmount,
         'divYield': divYield,
         'divDate': divDate,
-        })
+    })
+
+
+def terrainapp(request):
+
+    latitude = float(request.GET.get('lat', None))
+    longitude = float(request.GET.get('lon', None))
+    zoom = int(request.GET.get('zoom', None))
+
+    ytile = math.floor((1-math.log(math.tan(latitude*math.pi/180) + 1/math.cos(latitude*math.pi/180))/math.pi)/2 * math.pow(2,zoom))
+    xtile = math.floor((longitude+180)/360*math.pow(2,zoom))
+
+    endpoint = 'https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png'
+
+    full_url = endpoint.format(z=zoom, x=xtile, y=ytile)
+
+    response = requests.get(full_url)
+    img = Image.open(BytesIO(response.content))
+
+    pix = img.convert('RGB')
+
+    heights = {}
+    k = 0
+
+    for i in range(256):
+        for j in range(256):
+            r, g, b = pix.getpixel((i,j))
+            height = (r * 256 + g + b / 256) - 32768
+            heights[k] = height
+            # print(heights[k])
+            k += 1
+
+    # return render(request, 'projects/terrainapp.html', {'heights': heights})
+
+    return JsonResponse({"heights":heights}, status = 200)
